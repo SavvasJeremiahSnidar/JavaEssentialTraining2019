64fe491756dfe8a21773e26e8739f37052b3edfa
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-06-17 15:11:06 -0500

    Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var input = new Scanner(System.in);
+        System.out.println("Enter a numeric value: ");
+        float first = input.nextFloat();
+        System.out.println("Enter a numeric value: ");
+        float second = input.nextFloat();
+        System.out.println("The answer is: " + (first / second));
+
     }
 }
