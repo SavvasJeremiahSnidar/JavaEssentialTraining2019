ebe6fd1188477816b93337c3106331574a34d2ab
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-06-17 14:28:19 -0500

    Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
         var doubleValue = 10_000_000.53;
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number: " + numF.format(doubleValue));
+
+        var iF = NumberFormat.getIntegerInstance();
+        System.out.println("Integer: " + iF.format(doubleValue));
+
+        iF.setGroupingUsed(false);
+        System.out.println("Integer: " + iF.format(doubleValue));
+
+        var locale = new Locale("es");
+        var localeFormatter = NumberFormat.getNumberInstance(locale);
+        System.out.println("Integer: " + localeFormatter.format(doubleValue));
+
+        var currencyFormatter = NumberFormat.getCurrencyInstance();
+        System.out.println("Currency: " + currencyFormatter.format(doubleValue));
+
     }
 }
